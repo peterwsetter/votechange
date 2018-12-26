27d98778f4823510748a257898a4305f26b4c412
peterwsetter <peter.w.setter@gmail.com>
2018-12-26 16:29:53 +0000

    completed video

---
+++ make-video.R
@@ -0,0 +1,10 @@
+#' Copyright Peter W Setter 2018
+#' Create video of election analysis
+
+library(ari)
+
+ari_narrate(script = 'video-presentation.Rmd',
+            slides = 'video-presentation.html',
+            output = 'data-products/democrats-won-with-2016-vote-totals.mp4',
+            voice = 'Matthew'
+            )
